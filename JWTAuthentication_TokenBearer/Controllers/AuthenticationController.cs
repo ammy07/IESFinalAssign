749cdd61e7f5d4044817bdde571b108608221fa3
ammy07 <72196731+ammy07@users.noreply.github.com>
2020-12-15 02:02:20 +0530

    Add files via upload

---
+++ JWTAuthentication_TokenBearer/Controllers/AuthenticationController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using JWTAuthentication_TokenBarer.Services;
+using JWTAuthentication_TokenBearer.Models;
+
+namespace JWTAuthentication_TokenBearer.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class AuthenticationController : ControllerBase
+    {
+        IAuthenticateService _authenticateService;
+
+        public AuthenticationController(IAuthenticateService authenticateService)
+        {
+
+            _authenticateService = (IAuthenticateService)authenticateService;
+        }
+        [HttpPost]
+        public IActionResult Post([FromBody] Student model)
+        {
+            var student = _authenticateService.Authenticate(model.UserName, model.Password);
+            if (student == null)
+            {
+                return BadRequest(new { Message = "invalid username or password" });
+            }
+            else
+                return Ok(student);
+        }
+    }
+}
---
+++ JWTAuthentication_TokenBearer/Controllers/UsersController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace JWTAuthentication_TokenBearer.Controllers
+{
+    [Authorize(AuthenticationSchemes = JwtBearerDefaults.AuthenticationScheme)]
+    [ApiController]
+    [Route("[controller]")]
+    public class UsersController : ControllerBase
+    {
+
+        private readonly ILogger<UsersController> _logger;
+
+        public UsersController(ILogger<UsersController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpGet]
+        public List<Users.Result> Get()
+        {
+
+            Users.Root root =  JsonConvert.DeserializeObject<Users.Root>(Constants.json);
+            List<Users.Result> result = root.result;
+            return result; 
+
+        }
+
+        [HttpGet("{id}")]
+        public  List<Users.Result> GetUserById(string id)
+        {
+            
+            Users.Root root = JsonConvert.DeserializeObject<Users.Root>(Constants.json);
+            List<Users.Result> result = root.result;
+
+            foreach (var users in result)
+            {
+                if(users.id == id)
+                {
+                    List<Users.Result> newList = new List<Users.Result>();
+                    newList.Add(users);
+                   return newList;
+                }
+            }       
+            return null;
+        }
+    }
+}
